72abe87aed20bb02413fd3ce22baeb08e1fed940
paransharma <paransharma94@gmail.com>
2023-07-18 17:07:37 +0530

    whitespace cleanup 2

--- app/src/main/java/com/adsama/clevertapassignment/utils/BindingUtil.kt
+++ app/src/main/java/com/adsama/clevertapassignment/utils/BindingUtil.kt
@@ -7,13 +7,6 @@ import androidx.databinding.BindingAdapter
 import com.adsama.clevertapassignment.R
 import com.bumptech.glide.Glide
 
-@BindingAdapter("hundImage")
-fun setHundImage(imageView: ImageView, hundImageUrl: String?) {
-    Glide.with(imageView.context).load(hundImageUrl)
-        .error(AppCompatResources.getDrawable(imageView.context, R.drawable.empty_error))
-        .into(imageView)
-}
-
 @BindingAdapter("setSubmitButtonState")
 fun setSubmitButtonState(button: Button, string: String?) {
     if (string.isNullOrEmpty()) {
